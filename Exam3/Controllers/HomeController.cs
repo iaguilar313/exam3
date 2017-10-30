fe8a07560dd49281c6ecefe025beeb38b2df4f71
iaguilar313 <iaguilar@umich.edu>
2017-10-30 14:46:02 -0400

    first udpate

---
+++ Exam3/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exam3.Models;
+
+namespace Exam3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            northwindEntities NorthwindDB = new northwindEntities();
+            List<Order> AllOrders = NorthwindDB.Orders.ToList();
+
+            ViewBag.Orders = AllOrders;
+
+            return View();
+        }
+
+        public ActionResult AddNumbers(string Number1, string Number2)
+        {
+            int num1 = Int32.Parse(Number1);
+            int num2 = Int32.Parse(Number2);
+            ViewBag.result = num1 + num2;
+
+            return View("Result");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
